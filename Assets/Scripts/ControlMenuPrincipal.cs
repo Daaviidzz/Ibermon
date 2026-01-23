67935c1e7975c7508666272557a5bfd8c1bc607e
sergiocasin3@gmail.com <>
2026-01-23 10:52:05 +0100

    Arreglado el menu de la pantalla principal para que pase a la siguiente pantalla

--- Assets/Scripts/ControlMenuPrincipal.cs
+++ Assets/Scripts/ControlMenuPrincipal.cs
@@ -5,13 +5,13 @@ public class ControlMenuPrincipal : MonoBehaviour
 {
     void Update()
     {
-        //Para detectar si pulsa enter
-        if (Input.GetKeyDown(KeyCode.KeypadEnter))
+        //Para detectar si pulsa enter del teclado y el enter del teclado numerico
+        if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter))
         {
             //Como esta escena sería la 0 la del menú principal y queremos acceder a la del juego que sería la siguiente
             //la escena a la que queremos acceder será la 1
             SceneManager.LoadScene(1);
         }
-            
+
     }
 }
