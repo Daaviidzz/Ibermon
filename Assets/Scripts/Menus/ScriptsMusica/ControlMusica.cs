e80bebffb945d7fbfc0ed45abfcceb06ff9fe0cf
dballestero366@gmail.com <>
2026-01-31 13:33:35 +0100

    Intento Desesperado de arreglar el volumen( no va, hay que arreglarlo )

--- Assets/Scripts/Menus/ScriptsMusica/ControlMusica.cs
+++ Assets/Scripts/Menus/ScriptsMusica/ControlMusica.cs
@@ -1,42 +1,40 @@
 using UnityEngine;
-using UnityEngine.Audio;
+using UnityEngine.Audio; 
 
 public class ControlMusica : MonoBehaviour
 {
-    // Esto permite que el script sea accesible desde cualquier escena
     public static ControlMusica instance;
 
+    [Header("Configuracion de Audio")]
     [SerializeField] private AudioMixer audioMixer;
     private AudioSource audioSource;
 
+
+    //Para que no se destruya al cambiar de escena
     void Awake()
     {
+     
         if (instance == null)
         {
             instance = this;
-            DontDestroyOnLoad(gameObject); // Para que no se pare al cambiar de escena
+            DontDestroyOnLoad(gameObject);
             audioSource = GetComponent<AudioSource>();
         }
         else
         {
-            Destroy(gameObject); // Evita que se duplique la música si vuelves al menú
+            Destroy(gameObject);
         }
     }
-
-    // Controla el volumen de la música según el valor del slider
-    public void ControlMusicaVolumen(float sliderMusica)
+    public void ControlMusicaVolumen(float sliderValue)
     {
-        float volume = sliderMusica > 0 ? Mathf.Log10(sliderMusica) * 20 : -80f;
-        audioMixer.SetFloat("VolumenMusica", volume);
+        
+        float volume = sliderValue > 0.0001f ? Mathf.Log10(sliderValue) * 20 : -80f;
+        audioMixer.SetFloat("ControlMusica", volume);
     }
 
-    // Este método se llama cuando se inicia el juego desde el menú
     public void PararMusicaMenu()
     {
-        if (audioSource != null)
-        {
-            audioSource.Stop();
-        }
-        Destroy(gameObject); // Borramos el objeto del menú porque ya empezó el juego
+        if (audioSource != null) audioSource.Stop();
+        Destroy(gameObject);
     }
 }
